0567e804201c245d47f8d7ca9e57582bf3bbddfb
Max Woolf <max@minimaxir.com>
2020-04-18 09:52:08 -0700

    Metadata

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+long_description = """
+A robust tool for advanced AI text generation.
+"""
+
+
+setup(
+    name="aitextgen",
+    packages=["aitextgen"],  # this must be the same as the name above
+    version="0.1",
+    description="A robust tool for advanced AI text generation using Transformers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Max Woolf",
+    author_email="max@minimaxir.com",
+    url="https://github.com/minimaxir/aitextgen",
+    keywords=["wordcloud", "data visualization", "text cool stuff"],
+    classifiers=[],
+    license="MIT",
+    # entry_points={
+    #     'console_scripts': ['stylecloud=stylecloud.stylecloud:stylecloud_cli'],
+    # },
+    python_requires=">=3.6",
+    include_package_data=True,
+    install_requires=[
+        "transformers>=2.8.0",
+        "fire",
+        "msgpack",
+        "pytorch-lightning>=0.7.3",
+        "tqdm>=4.41.0",
+        "pyyaml",
+    ],
+)
